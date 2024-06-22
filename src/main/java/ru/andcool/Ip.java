614d01eeac0aa8ac92a720acf22a6c7930949b06
Andcool-Systems <drolll2007@gmail.com>
2024-06-22 13:32:02 +0300

    Migrated to InetAddress

--- src/main/java/ru/andcool/Ip.java
+++ src/main/java/ru/andcool/Ip.java
@@ -1,12 +1,10 @@
 package ru.andcool;
 
-import java.net.URI;
+import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Ip {
@@ -31,35 +29,12 @@ public class Ip {
         return null;
     }
 
-    private static final String IPV4_REGEX =
-            "\\b((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\\b";
 
-    private static final String IPV6_REGEX =
-            "\\b(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\\b";
-
-    private static final Pattern IPV4_PATTERN = Pattern.compile(IPV4_REGEX);
-    private static final Pattern IPV6_PATTERN = Pattern.compile(IPV6_REGEX);
-
-    public static boolean isValidIPv4(String ip) {
-        if (ip == null) {
-            return false;
-        }
-        Matcher matcher = IPV4_PATTERN.matcher(ip);
-        return matcher.matches();
-    }
-
-    public static boolean isValidIPv6(String ip) {
-        if (ip == null) {
-            return false;
-        }
-        Matcher matcher = IPV6_PATTERN.matcher(ip);
-        return matcher.matches();
-    }
-
-    public static String validateIp(String ip) {
-        if (isValidIPv4(ip)) {
+    public static String validateIp(String ip) throws UnknownHostException {
+        InetAddress address = InetAddress.getByName(ip);
+        if (address instanceof Inet4Address) {
             return "A";
-        } else if (isValidIPv6(ip)) {
+        } else if (address instanceof Inet6Address) {
             return "AAAA";
         }
         throw new RuntimeException("Cannot validate ip!");
